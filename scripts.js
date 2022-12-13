5f07c3c9d68554009961e73283c0ae4ad52360b0
sergioIgnatev <sergioignatev17@gmail.com>
2022-12-13 17:14:25 +0300

    routes

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let Here=(x)=>{
+return `<div style="color:red">HELLO THERE ${x}</div>`
+
+}
+
+const Home = { template: '<div>Home</div>' }
+const About = { template: Here("About") }
+const Phone = { template:Here('phone')}
+//
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path:'/phone',component: Phone}
+]
+
+const router = VueRouter.createRouter({
+  
+  history: VueRouter.createWebHashHistory(),
+  routes, 
+})
+
+
+const app = Vue.createApp({})
+
+app.use(router)
+
+app.mount('#app')
